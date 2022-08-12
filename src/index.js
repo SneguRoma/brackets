b73efc18fbd93a087f731cd1a9336f9a1df562fb
sneguroma <sneguroma@gmail.com>
2022-08-12 20:33:02 +0300

    feat: ready to push

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  for (i = 0; i < str.length; i++){
+    for (let n=0;n < bracketsConfig.length; n++){
+      let j = bracketsConfig[n];
+      if (str[i] === j[0]) {
+        arr.push(str[i]);         
+        if (str[i] !== j[1]) break;
+        else if (arr[arr.length-1] === arr[arr.length-2]) { arr.splice(-2); break;}
+            else  break;        
+        }
+      if (str[i] === j[1]){        
+        if (arr[arr.length-1] !== j[0]) {return false;} 
+        else arr.pop();  break;
+      }
+      else if (n !== bracketsConfig.length-1) {continue;}  else {return false;};      
+    }
+  }  
+  return arr.length === 0 ? true : false;
 }
